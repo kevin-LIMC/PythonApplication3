6238f7487779a55ca3635a4be6ae22fa6afa64be
Kevin Daniel Vazquez Vazquez <kevin09dvv@gmail.com>
2024-09-02 20:49:25 -0600

    Agregar archivos de proyecto.

---
+++ PythonApplication3.py
@@ -0,0 +1,16 @@
+aves =["Loro gris", "Paloma de diamante", "Coctel"]
+print ("Los valores de la lista antes de insertar: ")
+
+for ave in aves: 
+    print(ave,end=" ")
+print(" n")
+    
+aves.append("Mayna")
+aves.append("Periquitos")
+aves.append("Cacatua")
+print("Los valores de la lista despues de insertar: ")
+for ave in aves:
+   print(ave, end=" " )
+       
+   print (" n")
+
